c93fa26a84f38dfdc90415677f4759f0d7b7694e
SwordSong <1550185072@qq.com>
2021-12-14 22:56:20 +0800

    Site updated: 2021-12-14 22:56:17

--- js/next-boot.js
+++ js/next-boot.js
@@ -75,6 +75,7 @@ NexT.boot.registerEvents = function() {
 
 NexT.boot.refresh = function() {
 
+  
   /**
    * Register JS handlers by condition option.
    * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
